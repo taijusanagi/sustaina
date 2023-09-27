1de59457e3e0f9d2cba92796664c69d5b8423fd9
taijusanagi <taijusanagi@gmail.com>
2023-09-27 18:09:17 +0700

    migrate sandbox func to index

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,6 +2,16 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { useState } from "react";
 
+import { MsgDepositDeployment } from "@akashnetwork/akashjs/build/protobuf/akash/deployment/v1beta3/deploymentmsg";
+import {
+  getAkashTypeRegistry,
+  getTypeUrl,
+} from "@akashnetwork/akashjs/build/stargate/index";
+
+import { Registry } from "@cosmjs/proto-signing";
+import { SigningArchwayClient, ArchwayClient } from "@archwayhq/arch3.js";
+import { SigningStargateClient } from "@cosmjs/stargate";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -13,13 +23,126 @@ export default function Home() {
   const [archwayContractAddress, setArchwayContractAddress] = useState(
     "archway1tqfuly7tfcp7cfqfjq7nlv0xqhlpq3sjlcmgq2edxvky5r9k5qwq77lqkz"
   );
+  const [archwayContractRewardAddress, setArchwayContractRewardAddress] =
+    useState("");
 
-  const [archwayReward, setArchwayReward] = useState(0);
+  const [archwayReward, setArchwayReward] = useState("0");
   const [archwayWithdrawTx, setArchwayWithdrawTx] = useState("");
-  const [fund, setFund] = useState(0);
+  const [fund, setFund] = useState("500");
+  const [akashAddFundTx, setAkashAddFundTx] = useState("");
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalText, setModalText] = useState("");
+
+  const getRewardAddressAndSet = async () => {
+    const client = await ArchwayClient.connect(
+      "https://rpc.mainnet.archway.io"
+    );
+    const contractMetadata = await client.getContractMetadata(
+      archwayContractAddress
+    );
+    if (!contractMetadata || !contractMetadata.rewardsAddress) {
+      throw new Error("Contact metadata is invalid");
+    }
+    setArchwayContractRewardAddress(contractMetadata.rewardsAddress);
+  };
+
+  const getReward = async () => {
+    if (!archwayContractRewardAddress) {
+      alert("Archway contract reward address is invalid");
+      return;
+    }
+    const client = await ArchwayClient.connect(
+      "https://rpc.mainnet.archway.io"
+    );
+    const reward = await client.getAllRewardsRecords(
+      archwayContractRewardAddress
+    );
+    let amount = BigInt(0);
+    reward.map((v) => {
+      const [reward] = v.rewards;
+      amount = amount + BigInt(reward.amount);
+    });
+    setArchwayReward(amount.toString());
+  };
+
+  const claimReward = async () => {
+    if (!window.keplr) {
+      alert("Please install keplr extension");
+      return;
+    }
+
+    const chainId = "archway-1";
+    await window.keplr.enable(chainId);
+    const wallet = window.keplr.getOfflineSigner(chainId);
+    const [account] = await wallet.getAccounts();
+    if (account.address !== archwayContractRewardAddress) {
+      alert(
+        "Please use akash deployer account:" + archwayContractRewardAddress
+      );
+      return;
+    }
+    const client = await SigningArchwayClient.connectWithSigner(
+      "https://rpc.mainnet.archway.io",
+      wallet
+    );
+    const fee = {
+      amount: [],
+      gas: "100000",
+    };
+    const tx = await client.withdrawContractRewards(account.address, 0, fee);
+    setArchwayWithdrawTx(tx.transactionHash);
+  };
+
+  const addFund = async () => {
+    if (!window.keplr) {
+      alert("Please install keplr extension");
+      return;
+    }
+    const chainId = "akashnet-2";
+    await window.keplr.enable(chainId);
+    const wallet = window.keplr.getOfflineSigner(chainId);
+    const [account] = await wallet.getAccounts();
+    if (account.address !== akashDeploymentOwner) {
+      alert("Please use akash deployer account:" + akashDeploymentOwner);
+      return;
+    }
+    const message = MsgDepositDeployment.fromPartial({
+      id: {
+        owner: akashDeploymentOwner,
+        dseq: akashDeploymentDesc,
+      },
+      amount: {
+        denom: "uakt",
+        amount: fund,
+      },
+      depositor: account.address,
+    });
+    const msgAny = {
+      typeUrl: getTypeUrl(MsgDepositDeployment),
+      value: message,
+    };
+    const myRegistry = new Registry(getAkashTypeRegistry());
+    const rpcEndpoint = "https://rpc.akash.forbole.com:443";
+    const client = await SigningStargateClient.connectWithSigner(
+      rpcEndpoint,
+      wallet,
+      {
+        registry: myRegistry as any,
+      }
+    );
+    const fee = {
+      amount: [],
+      gas: "100000",
+    };
+    const tx = await client.signAndBroadcast(
+      account.address,
+      [msgAny],
+      fee,
+      archwayWithdrawTx || `ArchwayWithdrawTx: ${archwayWithdrawTx}`
+    );
+    setAkashAddFundTx(tx.transactionHash);
+  };
 
   return (
     <div
@@ -43,8 +166,8 @@ export default function Home() {
       </div>
 
       {/* Form Section */}
-      <div className="flex items-center justify-center flex-grow mb-24">
-        <div className="bg-white py-6 px-4 rounded-lg shadow-md max-w-lg w-full border border-green-200">
+      <div className="flex items-center justify-center flex-grow mb-24 p-2">
+        <div className="bg-white py-6 px-4 rounded-lg shadow-md max-w-lg w-full">
           <form className="space-y-4">
             {formMode === "input" && (
               <div>
@@ -63,7 +186,6 @@ export default function Home() {
                   className="mt-1 p-2 w-full border rounded-md text-gray-700"
                   placeholder="Enter Akash Deployment Owner"
                 />
-
                 <label
                   htmlFor="akashDeploymentDeseg"
                   className="block text-sm font-medium text-gray-700 mt-4"
@@ -79,7 +201,6 @@ export default function Home() {
                   className="mt-1 p-2 w-full border rounded-md text-gray-700"
                   placeholder="Enter Akash Deployment Deseg"
                 />
-
                 <label
                   htmlFor="archwayContractAddress"
                   className="block text-sm font-medium text-gray-700 mt-4"
@@ -95,10 +216,10 @@ export default function Home() {
                   className="mt-1 p-2 w-full border rounded-md text-gray-700"
                   placeholder="Enter Archway Contract Address"
                 />
-
                 <button
                   type="button"
-                  onClick={() => {
+                  onClick={async () => {
+                    await getRewardAddressAndSet();
                     setFormMode("dashboard");
                   }}
                   className="mt-4 w-full px-3 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50"
@@ -109,82 +230,103 @@ export default function Home() {
             )}
             {formMode === "dashboard" && (
               <div>
-                <div className="text-gray-700">
+                <div className="text-gray-700 break-all">
                   <label className="block text-sm font-medium text-gray-700">
-                    Akash Deployment Owner:
+                    Akash Deployment Owner
                   </label>
                   <p className="text-xs mt-1">{akashDeploymentOwner}</p>
-
                   <label className="block text-sm font-medium text-gray-700 mt-2">
-                    Akash Deployment Desc:
+                    Akash Deployment Desc
                   </label>
                   <p className="text-xs mt-1">{akashDeploymentDesc}</p>
 
                   <label className="block text-sm font-medium text-gray-700 mt-2">
-                    Archway Contract Address:
+                    Archway Contract Address
                   </label>
                   <p className="text-xs mt-1">{archwayContractAddress}</p>
+                  <label className="block text-sm font-medium text-gray-700 mt-2">
+                    Archway Contract Reward Address
+                  </label>
+                  <p className="text-xs mt-1">{archwayContractRewardAddress}</p>
                 </div>
-
                 <div className="mt-4">
                   <label className="block text-sm font-medium text-gray-700">
-                    Archway Reward:
+                    Archway Reward
                   </label>
                   <div className="flex items-center justify-between">
-                    <p className="text-xs mt-1"> {archwayReward}</p>
+                    <p className="text-xs mt-1"> {archwayReward} aarch</p>
                     <button
                       type="button"
                       className="px-3 py-1 border border-blue-300 text-blue-500 hover:bg-blue-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-opacity-50"
-                      // onClick handler for refreshing reward can be added here
+                      onClick={getReward}
                     >
                       🔄 Refresh
                     </button>
                   </div>
                 </div>
-
                 <button
                   type="button"
                   className="mt-4 w-full px-3 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50"
-                  // onClick handler for withdrawing reward can be added here
+                  onClick={claimReward}
                 >
                   Withdraw Reward in Archway
                 </button>
-
                 {archwayWithdrawTx && (
                   <div className="mt-4">
                     <label className="block font-medium">
                       Archway Withdraw Tx:
                     </label>
-                    <p className="text-xs mt-1"> {archwayWithdrawTx}</p>
+                    <p className="text-xs mt-1 break-all">
+                      <a
+                        href={`https://www.mintscan.io/archway/tx/${archwayWithdrawTx}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="block text-green-500 hover:underline mt-4 text-center"
+                      >
+                        {archwayWithdrawTx}
+                      </a>
+                    </p>
                   </div>
                 )}
-
                 <a
                   href="https://app.osmosis.zone/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="block text-green-500 hover:underline mt-4 text-center"
+                  className="block text-green-500 mt-4 text-center border border-green-500 rounded-md px-3 py-2"
                 >
                   Swap and Bridge
                 </a>
-
                 <label className="block text-sm font-medium text-gray-700 mt-4">
-                  Fund Amount
+                  Fund Amount (uakt)
                 </label>
                 <input
                   type="number"
                   value={fund}
-                  onChange={(e) => setFund(Number(e.target.value))}
+                  onChange={(e) => setFund(e.target.value)}
                   className="mt-1 p-2 w-full border rounded-md text-gray-700"
                 />
-
                 <button
                   type="button"
                   className="mt-4 w-full px-3 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50"
-                  // onClick handler for adding fund can be added here
+                  onClick={addFund}
                 >
                   Add Fund in Akash
                 </button>
+                {akashAddFundTx && (
+                  <div className="mt-4">
+                    <label className="block font-medium">Akash Fund Tx:</label>
+                    <p className="text-xs mt-1 break-all">
+                      <a
+                        href={`https://www.mintscan.io/akash/tx/${akashAddFundTx}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="block text-green-500 hover:underline mt-4 text-center"
+                      >
+                        {akashAddFundTx}
+                      </a>
+                    </p>
+                  </div>
+                )}
               </div>
             )}
           </form>
