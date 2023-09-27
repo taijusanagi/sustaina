6289b8d6664cffe2d46961c9379f5e062226851f
taijusanagi <taijusanagi@gmail.com>
2023-09-27 13:45:03 +0700

    add withdraw query func

--- app/src/pages/sandbox.tsx
+++ app/src/pages/sandbox.tsx
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 
-import { DepositDeploymentAuthorization } from "@akashnetwork/akashjs/build/protobuf/akash/deployment/v1beta3/authz";
 import { MsgDepositDeployment } from "@akashnetwork/akashjs/build/protobuf/akash/deployment/v1beta3/deploymentmsg";
 import {
   getAkashTypeRegistry,
@@ -8,6 +7,7 @@ import {
 } from "@akashnetwork/akashjs/build/stargate/index";
 
 import { Registry } from "@cosmjs/proto-signing";
+import { SigningArchwayClient, ArchwayClient } from "@archwayhq/arch3.js";
 import { SigningStargateClient } from "@cosmjs/stargate";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,70 +19,144 @@ declare global {
 }
 
 export default function Home() {
+  const akashDeployerOwner = "akash14utyn6v5t5xqh0eztaptzf2jm8juy0nzlgt96r";
+  const akashDeployerDeseg = "12973474";
+  const archwayContractAddress =
+    "archway1tqfuly7tfcp7cfqfjq7nlv0xqhlpq3sjlcmgq2edxvky5r9k5qwq77lqkz";
+
   return (
     <main className={`${inter.className}`}>
       <button
         onClick={async () => {
+          console.log("get reward");
           if (!window.keplr) {
             alert("Please install keplr extension");
-          } else {
-            const chainId = "akashnet-2";
-            await window.keplr.enable(chainId);
-            const wallet = window.keplr.getOfflineSigner(chainId);
-            console.log("wallet", wallet);
-            const [account] = await wallet.getAccounts();
-            console.log("account", account);
-            const message = MsgDepositDeployment.fromPartial({
-              id: {
-                owner: "akash14utyn6v5t5xqh0eztaptzf2jm8juy0nzlgt96r",
-                dseq: "12973474",
-              },
-              amount: {
-                denom: "uakt",
-                amount: "500",
-              },
-              depositor: "akash1mzctggcrjqpjqdlvus6skpdrq6jpylz6ruguru",
-            });
-
-            console.log("message", message);
-            const msgAny = {
-              typeUrl: getTypeUrl(MsgDepositDeployment),
-              value: message,
-            };
-            console.log("msgAny", msgAny);
-            const myRegistry = new Registry(getAkashTypeRegistry());
-            console.log("myRegistry", myRegistry);
-            const rpcEndpoint = "https://rpc.akashnet.net:443";
-            const client = await SigningStargateClient.connectWithSigner(
-              rpcEndpoint,
-              wallet,
-              {
-                registry: myRegistry as any,
-              }
-            );
-            console.log("client", client);
-            const fee = {
-              amount: [],
-              gas: "100000",
-            };
-            const tx = await client.signAndBroadcast(
-              account.address,
-              [msgAny],
-              fee
-            );
-            console.log("tx", tx);
+            return;
           }
+          const client = await ArchwayClient.connect(
+            "https://rpc.mainnet.archway.io"
+          );
+          const msg = {
+            get_count: {},
+          };
+          const { count } = await client.queryContractSmart(
+            archwayContractAddress,
+            msg
+          );
+          console.log("Counter: ", count);
+          const contractMetadata = await client.getContractMetadata(
+            archwayContractAddress
+          );
+          console.log("contractMetadata", contractMetadata);
+          if (!contractMetadata || !contractMetadata.rewardsAddress) {
+            return;
+          }
+          const reward = await client.getAllRewardsRecords(
+            contractMetadata.rewardsAddress
+          );
+          console.log("reward", reward);
         }}
       >
-        Add Fund
+        Get Reward
+      </button>
+      <button
+        onClick={async () => {
+          console.log("withdraw reward");
+          if (!window.keplr) {
+            alert("Please install keplr extension");
+            return;
+          }
+          const chainId = "archway-1";
+          await window.keplr.enable(chainId);
+          const wallet = window.keplr.getOfflineSigner(chainId);
+          const client = await SigningArchwayClient.connectWithSigner(
+            "https://rpc.mainnet.archway.io",
+            wallet
+          );
+          const [account] = await wallet.getAccounts();
+          const fee = {
+            amount: [],
+            gas: "100000",
+          };
+          const tx = await client.withdrawContractRewards(
+            account.address,
+            0,
+            fee
+          );
+          console.log("tx", tx);
+        }}
+      >
+        Withdraw Reward
       </button>
       <button
         onClick={() => {
-          const ms = DepositDeploymentAuthorization.fromPartial({});
-          console.log(ms);
+          console.log("withdraw reward");
+          if (!window.keplr) {
+            alert("Please install keplr extension");
+            return;
+          }
         }}
       >
-        Delegate
+        Convert Fund
+      </button>
+      <button
+        onClick={async () => {
+          console.log("add fund");
+          if (!window.keplr) {
+            alert("Please install keplr extension");
+            return;
+          }
+          const chainId = "akashnet-2";
+          await window.keplr.enable(chainId);
+          const wallet = window.keplr.getOfflineSigner(chainId);
+          console.log("wallet", wallet);
+          const [account] = await wallet.getAccounts();
+          console.log("account", account);
+          if (account.address !== akashDeployerOwner) {
+            alert("Please use akash deployer account:" + akashDeployerOwner);
+            return;
+          }
+          const message = MsgDepositDeployment.fromPartial({
+            id: {
+              owner: akashDeployerOwner,
+              dseq: akashDeployerDeseg,
+            },
+            amount: {
+              denom: "uakt",
+              amount: "500",
+            },
+            depositor: account.acount,
+          });
+          console.log("message", message);
+          const msgAny = {
+            typeUrl: getTypeUrl(MsgDepositDeployment),
+            value: message,
+          };
+          console.log("msgAny", msgAny);
+          const myRegistry = new Registry(getAkashTypeRegistry());
+          console.log("myRegistry", myRegistry);
+          const rpcEndpoint = "https://rpc.akashnet.net:443";
+          const client = await SigningStargateClient.connectWithSigner(
+            rpcEndpoint,
+            wallet,
+            {
+              registry: myRegistry as any,
+            }
+          );
+          console.log("client", client);
+          const fee = {
+            amount: [],
+            gas: "100000",
+          };
+          // const tx = await client.signAndBroadcast(
+          //   account.address,
+          //   [msgAny],
+          //   fee
+          // );
+          // console.log("tx", tx);
+        }}
+      >
+        Add Fund
       </button>
     </main>
   );
